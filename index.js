ff850a05db9ccbf22acfd5a21312ecca821bdf0f
benoitDD <benoitdegorsas@outlook.fr>
2019-04-30 11:20:28 +0200

    123457

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('so if gg ??')
+    res.send('so if gg a ??')
 })
 .listen(PORT, () => {
     console.log('app ready to listen to port ', PORT)
