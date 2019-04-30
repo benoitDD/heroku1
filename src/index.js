62104b732bb8149f5d048a8ae26aabfd8d44d462
benoitDD <benoitdegorsas@outlook.fr>
2019-04-30 15:05:29 +0200

    def

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('ca marche ?? ba oui !! :-) ' + process.env.FEFE)
+    res.send('ca marche ?? ba oui  MOUAIS ...!! :-) ' + process.env.FEFE)
 })
 .listen(PORT, () => {
     console.log('app ready to listen to port ', PORT)
